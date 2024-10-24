61323a4dd49cfaa72ed6565d5c04ada77708e6ee
viestamayriska <vristamayriska@gmail.com>
2024-10-24 16:57:33 +0700

    Jobsheet4Praktikum2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,16 +21,15 @@ class UserController extends Controller
     // Coba untuk membuat pengguna baru
     // UserModel::create($data);
     // $data = UserModel::all();
+       //$user = UserModel::firstWhere('level_id', 1);
+
+    // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        // abort(404);
+    // }); 
+    //  $user = UserModel::findOrFail(1); 
     // Mengambil data pengguna yang baru dibuat
-    //$user = UserModel::where('username', 'manager_tiga')->first(); // Ambil pengguna berdasarkan username
-    //  $user = UserModel::find(1); 
-    //$user = UserModel::firstWhere('level_id', 1);
-
-     $user = UserModel::findOr(1, ['username', 'nama'], function () {
-        abort(404);
-    });
+    $user = UserModel::where('username', 'manager9')->firstOrFail(); // Ambil pengguna berdasarkan username
     
-
        return view('user', ['data' => $user]);
     }
 }
