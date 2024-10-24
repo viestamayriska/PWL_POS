3c2b1ca846d696bd6784a4ce74908d76879c3a43
viestamayriska <vristamayriska@gmail.com>
2024-10-24 16:53:06 +0700

    Jobsheet4Praktikum2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,17 +10,28 @@ class UserController extends Controller
 {
     public function index()
     {
+        // Pastikan password ditetapkan dengan benar
         $data = [
             'level_id' => 2,
             'username' => 'manager_tiga',
             'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
+            'password' => Hash::make('12345') // Hash password
         ];
-        
-        UserModel::create($data);
-        
-        // Coba akses model UserModel
-        $user = UserModel::all(); // Ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+
+    // Coba untuk membuat pengguna baru
+    // UserModel::create($data);
+    // $data = UserModel::all();
+    // Mengambil data pengguna yang baru dibuat
+    //$user = UserModel::where('username', 'manager_tiga')->first(); // Ambil pengguna berdasarkan username
+    //  $user = UserModel::find(1); 
+    //$user = UserModel::firstWhere('level_id', 1);
+
+     $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        abort(404);
+    });
+    
+
+       return view('user', ['data' => $user]);
     }
 }
+
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,14 +12,14 @@
             <th>Name</th>
             <th>ID Level Pengguna</th>
         </tr>
-        @foreach ($data as $d)
-            <tr>
-                <td>{{ $d->user_id }}</td>
-                <td>{{ $d->username }}</td>
-                <td>{{ $d->nama }}</td>
-                <td>{{ $d->level_id }}</td>
-            </tr>
-        @endforeach
+     
+        <tr>
+            <td>{{ $data->user_id }}</td> <!-- Gunakan $d untuk setiap item dalam iterasi -->
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr>
+       
     </table>
 </body>
 </html>
