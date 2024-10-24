aee53419b9a66943edb145698203581dbd72e0d7
viestamayriska <vristamayriska@gmail.com>
2024-10-24 23:58:06 +0700

    Jobsheet5 - Praktikum4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,13 +23,22 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all();
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
+
+
     // Ambil data user dalam bentuk json untuk datatables
 public function list(Request $request)
 {
 $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
 ->with('level');
+
+// Filter data user berdasarkan level_id
+if ($request->level_id) {
+    $users->where('level_id', $request->level_id);
+}
+
 
 return DataTables::of($users)
  // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
@@ -47,6 +56,8 @@ return $btn;
 ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
 ->make(true);
 }
+
+
 // Menampilkan halaman form tambah user
 public function create()
 {
@@ -69,6 +80,7 @@ public function create()
         'activeMenu' => $activeMenu
     ]);
 }
+
 
 // Menyimpan data user baru
 public function store(Request $request)
@@ -94,6 +106,7 @@ public function store(Request $request)
     return redirect('/user')->with('success', 'Data user berhasil disimpan');
 }
 
+
 // Menampilkan detail user
 public function show(string $id)
 {
@@ -117,6 +130,7 @@ public function show(string $id)
         'activeMenu' => $activeMenu
     ]);
 }
+
 
 // Menampilkan halaman form edit user
 public function edit(string $id)
@@ -165,6 +179,7 @@ public function update(Request $request, string $id)
 
     return redirect('/user')->with('success', 'Data user berhasil diubah');
 }
+
 
 // Menghapus data user
 public function destroy(string $id)
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.template')
+
 @section('content')
     <div class="card card-outline card-primary">
         <div class="card-header">
@@ -14,6 +15,22 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter: </label>
+                        <div class="col-3">
+                            <select class="form-control" id="level_id" name="level_id" required>
+                                <option value="">- Semua -</option>
+                                @foreach($level as $item)
+                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Level Pengguna</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
                     <tr><th>ID</th><th>Username</th><th>Nama</th><th>Level Pengguna</th><th>Aksi</th></tr>
@@ -22,8 +39,10 @@
         </div>
     </div>
 @endsection
+
 @push('css')
 @endpush
+
 @push('js')
     <script>
         $(document).ready(function() {
@@ -34,6 +53,10 @@
                     "url": "{{ url('user/list') }}",
                     "dataType": "json",
                     "type": "POST"
+                    "type": "POST",
+                    "data" : function (d) {
+                        d.level_id = $('#level_id').val();
+                    }
                 },
                 columns: [
                     {
@@ -67,6 +90,9 @@
                         searchable: false
                     }
                 ]
+            });
+            $('#level_id').on('change', function() {
+                dataUser.ajax.reload();
             });
         });
     </script>
